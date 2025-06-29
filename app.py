5dbb2c47e82cb0537188be205eda2bc7596f7e39
nsumaa <dangarsumana05@gmail.com>
2025-06-29 12:22:02 +0530

    Initial Commit

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, request, render_template
+import pickle
+
+with open('model.pkl', 'rb') as f:
+    model = pickle.load(f)
+
+with open('vectorizer.pkl', 'rb') as f:
+    vectorizer = pickle.load(f)
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        message = request.form['message']
+        transformed_msg = vectorizer.transform([message])
+        prediction = model.predict(transformed_msg)[0]
+        result = "Spam ❌" if prediction else "Not Spam ✅"
+        return render_template('index.html', prediction=result, user_input=message)
+
+if __name__ == '__main__':
+    app.run(debug=True)
