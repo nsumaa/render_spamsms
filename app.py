5cbb084c33d09b3b7be6debcceb5956957fd2e33
nsumaa <dangarsumana05@gmail.com>
2025-07-09 00:41:29 +0530

    Fix PORT

--- app.py
+++ app.py
@@ -22,5 +22,8 @@ def predict():
         result = "Spam ❌" if prediction else "Not Spam ✅"
         return render_template('index.html', prediction=result, user_input=message)
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
